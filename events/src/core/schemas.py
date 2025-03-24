6ea6667fb31d68016eb51e5e4d2bacb3866c3b33
Moovlaze <max.moovlaze@gmail.com>
2025-03-25 00:50:23 +0300

    added new schema

--- events/src/core/schemas.py
+++ events/src/core/schemas.py
@@ -13,3 +13,11 @@ class SEvents(SEventsBase):
     event_date: datetime
     available_tickets: int
     ticket_price: int
+
+
+class SEventsUpdate(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    event_date: datetime | None = None
+    available_tickets: int | None = None
+    ticket_price: int | None = None
