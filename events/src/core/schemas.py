c8a9639b62c5b7af8b5be79d2a200bd907e86f94
Moovlaze <max.moovlaze@gmail.com>
2025-03-24 18:29:55 +0300

    change in schemas.py

--- events/src/core/schemas.py
+++ events/src/core/schemas.py
@@ -3,8 +3,11 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
-class SEvents(BaseModel):
+class SEventsBase(BaseModel):
     id: int
+
+
+class SEvents(SEventsBase):
     name: str
     description: str
     event_date: datetime
