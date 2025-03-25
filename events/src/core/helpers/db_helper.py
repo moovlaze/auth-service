41e189d696ccb62738d5a0ba06c8af5035cdf1cb
Moovlaze <max.moovlaze@gmail.com>
2025-03-25 14:26:59 +0300

    added new fild and method to model Events

--- events/src/core/helpers/db_helper.py
+++ events/src/core/helpers/db_helper.py
@@ -8,4 +8,5 @@ Session = sessionmaker(bind=engine)
 
 
 def init_db():
+    # Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
--- events/src/core/orm_models/events.py
+++ events/src/core/orm_models/events.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy import Column, Integer, String, DateTime, Index
 
 from .base import Base
@@ -12,5 +14,14 @@ class Events(Base):
     event_date = Column(DateTime)
     available_tickets = Column(Integer)
     ticket_price = Column(Integer)
+    delete_at = Column(DateTime, nullable=True)
 
-    __table_args__ = (Index("idx_events_event-date", "event_date"),)
+    __table_args__ = (
+        Index(
+            "idx_events_event_date",
+            "event_date",
+        ),
+    )
+
+    def soft_delete(self):
+        self.delete_at = datetime.now()
