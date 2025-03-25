e004d53bfef2a6bfbc419c168c4e23bcd9559c4f
Moovlaze <max.moovlaze@gmail.com>
2025-03-25 17:22:23 +0300

    added IEventRepository

---
+++ events/src/core/repository.py
@@ -0,0 +1,20 @@
+from typing import Protocol
+
+
+class IEventRepository(Protocol):
+    def send(self, operation: str, description: str) -> None:
+        pass
+
+
+class MyEventRepository:
+    def send(self, operation: str, description: str) -> None:
+        with open(
+            r"C:\my\projects\auth-service\events\src\core\events_repository\events.txt",
+            mode="a",
+            encoding="UTF-8",
+        ) as fl:
+            fl.write(f"----------\n{operation}\n{description}\n----------")
+
+
+def get_event_repository() -> IEventRepository:
+    return MyEventRepository()
